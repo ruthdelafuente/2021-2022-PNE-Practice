0eb196210f35b84c5e28fcc3d543abbfab4c83c7
ruuthdlf <ruth.delafuente@alumnos.upm.es>
2022-04-27 18:42:08 +0200

    practice 7

---
+++ P7/ex_1.py
@@ -0,0 +1,37 @@
+# -- Example of a client that uses the HTTP.client library
+# -- for requesting the main page from the server
+import http.client
+import json
+
+SERVER = 'rest.ensembl.org'
+ENDPOINT = '/info/ping'
+PARAMS = '?content-type=application/json'
+
+print(f"\nConnecting to server: {SERVER}\n")
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+try:
+    conn.request("GET", ENDPOINT + PARAMS) #we dont need the server
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode("utf-8")
+data1 = json.loads(data1)
+if data1['ping'] == 1: #json.loads converts it into a integers for us
+    print("PING OK!! The data base is running")
+else:
+    print("ERROR!!! The data base is not running")
+# -- Print the received data
+print(f"CONTENT: {data1}")
